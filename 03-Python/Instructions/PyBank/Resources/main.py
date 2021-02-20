431491c8445717b43b54e2e06dc63d57c7df38de
Tejas Patel <Tejas@Tejass-MacBook-Pro.local>
2021-02-20 10:44:35 -0500

    HWPython

---
+++ 03-Python/Instructions/PyBank/Resources/main.py
@@ -0,0 +1,70 @@
+import os, csv
+from pathlib import Path 
+
+# Declare file location
+input_file = os.path.join('budget_data.csv')
+
+# Create empty lists to iterate through the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
+ 
+
+with open(input_file) as budget:
+
+     # Store the budget_data.csv in the variable csvreader
+    csvreader = csv.reader(budget,delimiter=",") 
+
+    
+    header = next(csvreader)  
+
+    # Use loop through rows in the stored file contents
+    for row in csvreader: 
+
+        # Append the total months and total profit
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+
+    # Use loop through the profits in order to get the monthly change in profits
+    for i in range(len(total_profit)-1):
+        
+        # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        
+# Collect the max and min of the the montly profit change list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
+
+#We use the plus 1 at the end since month associated with change is the + 1 month or next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+# Print Statements
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+# Output files
+output_file = Path("Financial_Analysis_Summary.txt")
+
+with open(output_file,"w") as file:
+    
+# Write methods to print to Financial_Analysis_Summary 
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
